6e4f3a2aff57202827e7f589ae48deba4410ca9e
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-07-18 02:07:24 +0900

    B - Bouzu Mekuri
    問題文
    N枚のカードからなる山札があります。
    それぞれのカードは、「良いカード」か「悪いカード」かのどちらかです。
    
    高橋君と青木君は、この山札を使って対戦ゲームをします。
    このゲームでは、2人は交互に山札の一番上のカードを引いて、そのカードを食べます。
    先に悪いカードを食べたプレイヤーの負けです。
    （ここで、山札には少なくとも 1枚の悪いカードが含まれていることが保証されます。）
    
    0 と 1 からなる文字列 S が与えられます。
    i=1,2,…,N について、
    
    S のi 文字目が 0 のとき、山札の上からi 番目のカードが良いカードであることを表します。
    S のi文字目が 1 のとき、山札の上からi 番目のカードが悪いカードであることを表します。
    高橋君が先手でゲームを始めるとき、高橋君と青木君のどちらが負けるかを答えてください。

---
+++ B_BouzuMekuri.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _210717_B_BouzuMekuri
+{
+    class B_BouzuMekuri
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            //char[] array_S = new char[N];
+            string S = Console.ReadLine();
+            //array_S = S.ToCharArray();
+
+            int num = S.IndexOf("1") ;
+
+            if ((num % 2) == 0)
+            {
+                Console.WriteLine("Takahashi");
+            }
+            else if ((num % 2) == 1)
+            {
+                Console.WriteLine("Aoki");
+            }
+
+            /*for (int i = 0; i < N; i++)
+            {
+                if (array_S[i].ToString() == "1")
+                {
+                    if ((i % 2) == 0)
+                    {
+                        Console.WriteLine("Takahashi");
+                        break;
+                    }
+                    else if ((i % 2) == 1)
+                    {
+                        Console.WriteLine("Aoki");
+                        break;
+                    }
+
+
+                }
+            }*/
+        }
+    }
+}
