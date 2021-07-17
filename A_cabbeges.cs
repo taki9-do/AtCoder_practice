c20d9e0a34e8521818c2c8e785299a704618b693
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-07-18 01:43:44 +0900

    A - Cabbages
    問題文
    
    高橋君はキャベツ屋さんにやってきました。
    キャベツ屋さんでは、 キャベツを1個X 円で買うことができます。
    ただし、キャベツをA 個よりも多く買う場合、A+1個目以降に買うキャベツについては 
    1個Y円で買うことができます。（ここで、Y<Xが保証されます。）
    高橋君がキャベツを N 個買うために必要な金額を出力してください。
    
    制約
    1≤N≤10^5
    1≤A≤10^5
    1≤Y<X≤100
    入力はすべて整数
    
    入力
    入力は以下の形式で標準入力から与えられる。
    
    N A X Y
    
    出力
    高橋君がN 個のキャベツを買うために必要な金額を出力せよ。

---
+++ A_cabbeges.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _210717_1_cabbages
+{
+    class A_cabbeges
+    {
+        static void Main(string[] args)
+        {
+
+            string[] input = Console.ReadLine().Split(' ');
+            int N = int.Parse(input[0]);
+            int A = int.Parse(input[1]);
+            int X = int.Parse(input[2]);
+            int Y = int.Parse(input[3]);
+
+            int sum = 0;
+
+            for(int i = 0; i < N; i++)
+            {
+                if (i < A)
+                {
+                    sum += X;
+                }
+                else if (i >= A)
+                {
+                    sum += Y;
+                }
+            }
+
+            Console.Write(sum);
+
+        }
+    }
+}
