cb2370fa503156e5fb0bb3aba869a2efb17bfe2d
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-07-18 18:09:53 +0900

    C - Colorful Candies
    問題文
    N 個のキャンディが左右一列に並んでいます。
    それぞれのキャンディは、色1、色2、…、色10^9の、
    10^9 種類の色のうちいずれかの色をしています。
    i=1,2,…,N について、左から i 番目のキャンディの色は色 ci です。
    
    高橋君は並んでいるキャンディのうち、連続して並んだ 
    K 個のキャンディをもらうことができます。
    すなわち、1≤i≤N−K+1 を満たす整数i を選んで、 
    左から i 番目、i+1 番目、i+2 番目、…、i+K−1 番目のキャンディをもらうことができます。
    
    高橋君はいろいろな色のキャンディを食べたいので、 もらうキャンディに含まれる色の種類数が多いほどうれしい気持ちになります。
    高橋君がもらうキャンディに含まれる色の種類数の最大値を出力してください。

---
+++ C_ColorfulCandies.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Console;
+
+namespace _210717_C_ColorfulCandies
+{
+    class C_ColorfulCandies
+    {
+        static void Main(string[] args)
+        {
+            //N,Kの入力
+            string[] input1 = Console.ReadLine().Split(' ');
+            int N = int.Parse(input1[0]);
+            int K = int.Parse(input1[1]);
+            input1 = null;
+
+            //Cの入力
+            string[] input2 = Console.ReadLine().Split(' ');
+            int[] C = new int[N];
+            for(int i = 0; i <N; i++)
+            {
+                C[i]=(int.Parse(input2[i]));
+            }
+            input2 = null;
+
+            //連想配列dictionaryにC[0~(K-1)]の種類数を格納する。
+            //'1'が2つなら'2','2'が１つなら'1'をそれぞれkeyとvalueとする。
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            for(int i = 0; i < K; i++)
+            {
+                
+                if (dic.ContainsKey(C[i])==false)//重複がなければ要素追加
+                {
+                    dic.Add(C[i], 1);
+                }
+                else if (dic.ContainsKey(C[i]))//重複があれば+1
+                {
+                    dic[C[i]]++;
+                }
+            }
+            int ans = dic.Count;//種類数の初期化
+
+            //尺取り探索を活用。
+            for(int i = K; i < N; i++)
+            {
+                //C[i(K)]（次の要素）を探索
+                if (dic.ContainsKey(C[i]) == false)
+                {
+                    dic.Add(C[i], 1);
+                }
+                else if (dic.ContainsKey(C[i]))
+                {
+                    dic[C[i]]++;
+                }
+
+                //C[i-K]（最後に探索した要素）を-1または削除
+                if (dic.ContainsKey(C[i-K]) == true)
+                {
+                    dic[C[i - K]]--;
+                    if (dic[C[i - K]] == 0)
+                    {
+                        dic.Remove(C[i - K]);
+                    }
+
+                }
+                
+                //最大値の更新
+                ans = Math.Max(ans, (dic.Count));
+            }
+
+
+            Console.WriteLine(ans);
+
+
+        }
+    }
+}
