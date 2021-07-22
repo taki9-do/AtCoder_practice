98ceb3a82c40be799d6436f40a245fbae62305fa
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-07-23 03:04:06 +0900

    A - Arithmetic Sequence
    問題文
    3項からなる整数列 
    A=(A1,A2,A3) が与えられます。あなたはこの数列に対して、次の操作を何回でも行うことができます：
    
    i∈{1,2,3} をひとつ選び、Ai に1 を加える。
    数列 A を等差数列にするために必要な操作回数の最小値を求めてください。ただし、数列 
    A=(A1,A2,A3) が等差数列であるとは、A2−A1=A3−A2 が成り立つことを意味します。
    
    制約
    1≤A1,A2,A3≤10＾15

---
+++ A_ArithmeticSequence.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _210718_A_ArithmeticSequence
+{
+    class A_ArithmeticSequence
+    {
+        static void Main(string[] args)
+        {
+            //オーバーフロー防ぐためdouble
+            string[] input = Console.ReadLine().Split(' ');
+            double A1 = double.Parse(input[0]);
+            double A2 = double.Parse(input[1]);
+            double A3 = double.Parse(input[2]);
+
+            double count = 0;
+            double ans = sub_1(A1, A2, A3);
+
+            double temp = 0;
+
+            if (ans < 0)
+            {
+                //小数点切り捨てはmath.floor
+                temp = Math.Floor((1 - ans) / 2);
+            }
+            else if (ans >= 0)
+            {
+                temp = 0;
+            }
+
+            count = ans + 3 * temp;
+
+            Console.WriteLine(count);
+            
+        }
+
+
+        static double sub_1(double a1,double a2,double a3)
+        {
+           double ans = (2 * a2) - a1 - a3;
+
+            return ans;
+        }
+    }
+}
