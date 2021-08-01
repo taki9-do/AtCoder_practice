5a85a18dfe54fe21b2b8436a679f23355273cb07
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-08-01 17:51:33 +0900

    B - Weak Password
    問題文
    4 桁の暗証番号 
    X1X2X3X4
     が与えられます。 番号は先頭の桁が 0 であることもあり得ます。
     暗証番号は以下のいずれかの条件をみたすとき弱い暗証番号と呼ばれます。
    
    4 桁とも同じ数字である。1≤i≤3 をみたす任意の整数i について、 
    Xi+1 が、Xi の次の数字である。 ただし、0≤j≤8 についてj の次の数字は j+1 であり、9 の次の数字は0 である。
    与えられた暗証番号が弱い暗証番号ならば Weak を、そうでないならば Strong を出力してください。

---
+++ B_WeakPassword.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace _210731_B_WeakPassword
+{
+    class B_WeakPassword
+    {
+        static void Main(string[] args)
+        {
+
+            int input = int.Parse(Console.ReadLine());
+
+            int temp = input;
+            int count = 0;
+            int[] X = new int[4];
+
+            while (temp > 0)
+            {
+                X[3-count] = (temp % 10);
+                temp /= 10;
+                count++;
+            }
+
+            bool all_same = false;
+            bool stairs = false;
+
+            if (X[0] == X[1])
+            {
+                if (X[1] == X[2])
+                {
+                    if (X[2] == X[3])
+                    {
+                        all_same = true;
+                    }
+                }
+            }
+
+            if (((X[0] + 1) % 10) == X[1]) 
+            {
+                if (((X[1] + 1) % 10) == X[2])
+                {
+                    if (((X[2] + 1) % 10) == X[3])
+                    {
+                        stairs = true;
+                    }
+                }
+
+            }
+
+            
+
+            if (all_same == true || stairs == true)
+            {
+                Console.WriteLine("Weak");
+            }
+            else
+            {
+                Console.WriteLine("Strong");
+
+            }
+
+        }
+
+    }
+}
