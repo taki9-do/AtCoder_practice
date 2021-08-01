79819b61c113aacd3a0248e4e200fda34b5455fe
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-08-01 19:22:25 +0900

    C - Min Difference
    問題文
    それぞれ N 個、M 個の正整数からなる2 つの数列A=(A1,A2,…,AN) とB=(B1,…,BM) が与えられます。
    
    それぞれの数列から 1 つずつ要素を選んだときの2 つの値の差の最小値、すなわち、min1≤i≤N
      
    min1≤j≤M |Ai−Bj| を求めてください。
    
    制約
    1≤N,M≤2×10^5
    1≤Ai≤10^9
    1≤Bi≤10^9
    入力は全て整数である。

---
+++ C_MinDifference.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _210731_C_MinDifference
+{
+    class C_MinDifference
+    {
+        static void Main(string[] args)
+        {
+
+            //配列の長さの入力
+            string[] input = Console.ReadLine().Split(' ');
+            int N = int.Parse(input[0]);
+            int M = int.Parse(input[1]);
+
+            //配列Aの入力
+            int[] A = new int[N];
+            string[] input1 = Console.ReadLine().Split(' ');
+            for(int i = 0; i < A.Length; i++)
+            {
+                A[i] = int.Parse(input1[i]);
+            }
+
+            //配列Bへの入力
+            int[] B = new int[M];
+            string[] input2 = Console.ReadLine().Split(' ');
+            for (int i = 0; i < B.Length; i++)
+            {
+                B[i] = int.Parse(input2[i]);
+            }
+
+            //配列のソート
+            Array.Sort(A);
+            Array.Sort(B);
+
+            int min = 1010000000;
+
+            int ai = 0, bi = 0;
+
+            while (ai < N && bi < M)
+            {
+                min = Math.Min(min, Math.Abs(A[ai] - B[bi]));
+                if (A[ai] < B[bi]) ai++;
+                else bi++;
+            }
+
+            Console.WriteLine(min);
+
+        }
+    }
+}
