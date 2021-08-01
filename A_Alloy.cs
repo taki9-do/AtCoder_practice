4e691bb6f8c2c04ef61185c63e2177f1feaaee9b
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-08-01 17:49:44 +0900

    A - Alloy
    問題文
    高橋くんは 
    A グラムの純金と B グラムの純銀 (0≤A,B, 0<A+B)
     をよく溶かした上で混ぜ合わせ、新たな金属を生成しました。
    
    生成された金属は「純金」「純銀」「合金」のいずれでしょうか？
    
    なお、生成された金属は
    
    0<A かつB=0 なら「純金」
    A=0 かつ0<B なら「純銀」
    0<A かつ0<B なら「合金」
    であるとみなします。

---
+++ A_Alloy.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _210731_A_Alloy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] input = Console.ReadLine().Split(' ');
+            int A = int.Parse(input[0]);
+            int B = int.Parse(input[1]);
+
+            if (A > 0 || B > 0)
+            {
+                if (A == 0)
+                {
+                    Console.WriteLine("Silver");
+
+                }
+                else if (B == 0)
+                {
+                    Console.WriteLine("Gold");
+
+                }
+                else
+                {
+                    Console.WriteLine("Alloy");
+
+                }
+            }
+
+            
+
+        }
+    }
+}
