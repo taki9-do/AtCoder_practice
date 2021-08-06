31833c4a648ea856bedf91ea993ce7b5cac1d579
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-08-07 01:35:56 +0900

    D-QueryingMultiset【優先付きキュー】
    問題文
    高橋君は何も書かれていないたくさんのボールと 1 つの袋を持っています。 最初、袋は空で、高橋君はQ 回の操作を行います。
     それぞれの操作は以下の 3 種類のうちのいずれかです。
    
    操作 1 : まだ何も書かれていないボール 1 つに整数Xi を書き込み、袋に入れる。
    操作 2 : 袋に入っているすべてのボールについて、そこに書かれている数を、それにXi を加えたものに書き換える。
    操作 3 : 袋に入っているボールのうち書かれている数が最小のもの（複数ある場合はそのうちの1 つ）を取り出し、そこに書かれている数を記録する。その後、そのボールを捨てる。
    1≤i≤Q についてi 回目の操作の種類Pi および操作1  2 におけるXi の値が与えられるので、操作3 において記録された数を順に出力してください。
    
    入力例
    5
    1 3
    1 5
    3
    2 2
    3
    
    出力例
    3
    7

---
+++ 210803_D_QueryingMultiset.cpp
@@ -0,0 +1,46 @@
+﻿
+
+#include <iostream>
+#include<queue>
+
+
+using namespace std;
+
+#define ll long long
+#define rep(i,n) for(int i=0;i<n;i++)
+
+int main()
+{
+    priority_queue<ll, vector<ll>, greater<ll> >pq;
+
+    int Q_depth, label;
+    cin >> Q_depth;
+
+    ll x;
+
+    ll add_number = 0;
+    ll min_value;
+
+    for (int i = 0;i < Q_depth;i++) {
+        cin >> label;
+
+        if (label == 1) {
+            cin >> x;
+
+            pq.push(x - add_number);
+
+        }
+        else if (label == 2) {
+            cin >> x;
+            add_number += x;
+        }
+        else if (label == 3) {
+
+            min_value = pq.top();
+            cout << min_value + add_number << endl;
+            pq.pop();
+        }
+    }
+
+    
+}
