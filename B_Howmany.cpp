dc4617d99ccdd3b7aed894708e8188bfa7ca4ee8
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-08-16 21:55:18 +0900

    B - How many?
    問題文
    a+b+c≤S かつa×b×c≤T を満たす非負整数の組 
    (a,b,c) はいくつありますか？
    
    制約
    0≤S≤100
    0≤T≤10000
    S,T は整数である。
    
    入力例 1
    1 0
    出力
    4

---
+++ B_Howmany.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    // Your code here!
+    int S, T, count=0;
+
+    cin >> S >> T;
+
+    for (int i = 0;i <= S;i++) {
+
+        for (int j = 0;j <= (S - i);j++) {
+
+            for (int k = 0;k <= (S - i - j);k++) {
+
+                if ((i * j * k) <= T) {
+                    //cout<<i<<j<<k<<endl;
+                    count++;
+                }
+            }
+        }
+    }
+
+    cout << count;
+
+
+
+}
