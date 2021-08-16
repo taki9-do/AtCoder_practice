ed86a751540fc00f468216fee76c06d0c3356aec
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-08-16 22:32:54 +0900

    C - Distribution
    問題文
    N 人のすぬけ君が円周上に並んでおり、反時計回りに 1,2,...,N の番号がついています。
    
    i(1≤i≤N) 番目のすぬけ君は時刻t に宝石をもらうとSi 単位時間後、すなわち時刻 t+Si にその宝石を(i+1) 番目のすぬけ君に渡します。ただし、
    (N+1) 番目のすぬけ君とは1 番目のすぬけ君のことを指すとします。
    
    また、高橋君は時刻 Ti にi 番目のすぬけ君に宝石を渡します。
    全ての i(1≤i≤N) について、i 番目のすぬけ君が初めて宝石をもらう時刻を求めてください。なお、宝石の受け渡しにかかる時間は無視できるものとします。
    
    制約
    1≤N≤200000
    1≤Si,Ti≤10^9
    入力は全て整数である。
    
    入力
    N
    S1 S2 … SN
    T1 T2 … TN
    
    出力
    答え

---
+++ C_Distribution.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include<vector>
+
+using namespace std;
+
+#define ll long long
+
+int main(void) {
+    // Your code here!
+    int N;
+    cin >> N;
+
+    vector<ll> S(N), T(N);
+
+    for (int i = 0;i < N;i++) cin >> S[i];
+
+    for (int i = 0;i < N;i++) cin >> T[i];
+
+    vector<ll> Recieve_T = T;
+
+    for (int i = 0;i < N*2;i++) {
+        Recieve_T[(i + 1) % N] = min(Recieve_T[(i + 1) % N], Recieve_T[i % N] + S[i % N]);
+    }
+
+    for (int i = 0;i < N;i++) cout << Recieve_T[i] << endl;
+
+
+}
