4f5dd450fce0fa5308e95f055bd2c77e7f035fe0
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-08-22 13:45:19 +0900

    001 【2分探索法】
    長さ L [cm] のようかんがあります。
    N 個の切れ目が付けられており、左から i 番目の切れ目は左から A[i] [cm] の位置にあります。
    
    あなたは、N 個の切れ目のうち K 個を選び、ようかんを K+1 個のピースに分割したいです。そこで、以下の値をスコアとします。
    ・K+1 個のピースのうち、最も短いものの長さ
    スコアが最大になるように分割する場合に得られるスコアを求めよ。
    
    【制約】
    ・1 ≦ K ≦ N ≦ 10^5
    ・0 < A[1] < A[2] < ... < L ≦ 10^9
    ・入力はすべて整数
    
    【入力形式】
    ../sample/001.txt 参照

---
+++ 210821_001.cpp
@@ -0,0 +1,45 @@
+﻿// 210821_001.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+#include <vector>
+
+#define ll long long
+using namespace std;
+
+bool solve(ll M,ll N,ll L,ll K,vector<ll> A) {
+    ll cnt = 0, pre = 0;
+    for (int i = 1; i <= N; i++) {
+        if (A[i] - pre >= M && L - A[i] >= M) {
+            cnt += 1;
+            pre = A[i];
+        }
+    }
+    if (cnt >= K) return trues;
+    return false;
+}
+
+int main()
+{
+    // Step #1. 入力
+    ll N, L, K;
+    cin >> N >> L;
+    cin >> K;
+    vector<ll> A(N);
+    for (int i = 0;i < N;i++) cin >> A[i];
+
+    // Step #2. 答えで二分探索（めぐる式二分探索法）
+    // https://qiita.com/drken/items/97e37dd6143e33a64c8c
+    ll left = -1;
+    ll right = L + 1;
+    while (right - left > 1) {
+        ll mid = left + (right - left) / 2;
+        if (solve(mid,N,L,K,A) == false) {
+            right = mid;
+        }
+        else left = mid;
+    }
+    cout << left << endl;
+    return 0;
+
+}
